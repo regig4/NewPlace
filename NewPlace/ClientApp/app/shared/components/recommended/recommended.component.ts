7ff53e25190997425373c3df861c4fa6829f840d
Bartosz <bartosz.buras@outlook.com>
2018-04-08 06:34:01 +0200

    create new feature branch

---
+++ NewPlace/ClientApp/app/shared/components/recommended/recommended.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from "@angular/core";
+import { RecomendationsService } from "../../services/recomendations.service";
+
+
+@Component({
+    selector: 'recommended'
+})
+export class RecommendedComponent {
+    public recomendations: string[];
+
+    constructor(private service: RecomendationsService) {
+       
+    }
+}
